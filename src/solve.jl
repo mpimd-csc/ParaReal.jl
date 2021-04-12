8ce13e494b80af9d94f808cb25c7c510df036cf6
Jonas Schulze <jschulze@mpi-magdeburg.mpg.de>
2021-04-12 16:34:46 +0200

    Make lower bound nconverged configurable

--- src/solve.jl
+++ src/solve.jl
@@ -1,8 +1,21 @@
+"""
+    solve(prob, alg::ParaReal.Algorithm; kwargs...)
+
+Solve `prob` using the parareal scheme `alg`.
+Supported keyword arguments:
+
+* `maxiters = 10`: maximum number of refinements
+* `tol = 1e-5`: relative error bound to judge about preliminary convergence
+* `nconverged = 2`: lower bound on successive converged refinements
+
+Only if `nconverged` successive refinements show a relative change of
+at most `tol`, the corresponding time slice is considered convergent.
+"""
 function solve(
     prob,
     alg::Algorithm;
     workers = workers(),
-    maxiters = 10,
+    kwargs...
     )
 
     D.myid() in workers &&
@@ -16,7 +29,7 @@ function solve(
     pipeline = init_pipeline(workers)
 
     @debug "Starting worker tasks"
-    run_pipeline!(pipeline, prob, alg, maxiters=maxiters)
+    run_pipeline!(pipeline, prob, alg; kwargs...)
 
     @debug "Collecting local solutions"
     sol = collect_solutions(pipeline)
--- src/stages.jl
+++ src/stages.jl
@@ -1,12 +1,11 @@
 function execute_stage(prob,
                 alg::Algorithm,
                 config::StageConfig;
-                maxiters = 10,
-                tol = 1e-5,
+                kwargs...
                )
 
     try
-        _execute_stage(prob, alg, config; maxiters=maxiters, tol=tol)
+        _execute_stage(prob, alg, config; kwargs...)
     catch
         _send_status_update(config, :Failed)
         rethrow()
@@ -20,6 +19,7 @@ function _execute_stage(
     config::StageConfig;
     maxiters = 10,
     tol = 1e-5,
+    nconverged = 2,
 )
     _send_status_update(config, :Started)
     @unpack n, N, prev, next, results = config
@@ -30,7 +30,7 @@ function _execute_stage(
     tspan = local_tspan(n, N, prob.tspan)
 
     @debug "Waiting for data" n pid=D.myid() tid=T.threadid()
-    nconverged = 0
+    _nconverged = 0
     u′ = u = u_coarse = u_fine = nothing
     local k, msg, fsol, converged
     for outer k in 1:min(n, K)
@@ -50,8 +50,12 @@ function _execute_stage(
 
         # If the refined solution fulfills the convergence criterion,
         # perform a few iterations more to smooth out some more errors.
-        nconverged += sol_converged(u′, u; tol=tol)
-        converged = nconverged >= 2
+        if sol_converged(u′, u; tol=tol)
+            _nconverged += 1
+        else
+            _nconverged = 0
+        end
+        converged = _nconverged >= nconverged
 
         # Send correction of coarse solution on to the next stage
         cancelled = send_val(config, u, converged)
