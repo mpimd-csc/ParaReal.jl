1b499200f9a9ed79732822d09ccc60d1f3a2d9e8
Jonas Schulze <jschulze@mpi-magdeburg.mpg.de>
2021-10-25 16:05:42 +0200

    Automatically instantiate environment

---
+++ bench/script/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J init
+#SBATCH -o %x-%j.out
+#SBATCH -e %x-%j.err
+#SBATCH --time=15:00
+#SBATCH --partition=short
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=jschulze@mpi-magdeburg.mpg.de
+
+set -e
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
+export MKL_ENABLE_INSTRUCTIONS=AVX2
+export JULIA_PROJECT=@.
+
+module load apps/julia/1.6
+
+julia -e 'using Pkg; Pkg.instantiate()'
